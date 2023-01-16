ea36a34bd2c68fcc6bdb71b797b901d4952c3cba
simer <simerpreet2844@gmail.com>
2023-01-16 11:21:24 +0530

    we have apply some more css to dialog box and use state to toggle login and signup also make login functionality working with database, we have an error of unique key in banner and navabar now have removed by giving key and idx to it

--- Server/index.js
+++ Server/index.js
@@ -1,14 +1,20 @@
 import express from 'express';
 import dotenv from 'dotenv'
-
-
+import cors from 'cors'
 import Connection from './database/db.js';
 import DefaultData from './default.js';
+import bodyParser from 'body-parser';
+
+import Router from './routes/route.js'
 
 const app = express();
 
 dotenv.config();
 
+app.use(cors())
+app.use(bodyParser.json({extended: true}))
+app.use(bodyParser.urlencoded({extended: true}))
+app.use('/', Router)
 
 const PORT = 8000;
 
